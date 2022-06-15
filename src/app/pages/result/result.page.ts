074863f05d512e1f346aa74497a79abf3c0f5a16
lokin prasad <lokin.217@gmail.com>
2022-06-15 21:19:57 +0200

    Getting the distance value in console

--- src/app/pages/result/result.page.ts
+++ src/app/pages/result/result.page.ts
@@ -8,6 +8,7 @@ import {
   MapGeocoder,
   MapGeocoderResponse,
 } from '@angular/google-maps';
+import {Platform} from '@ionic/angular';
 
 declare var google: any;
 
@@ -27,13 +28,84 @@ export class ResultPage implements OnInit {
     private geo: Geolocation,
     private router: Router,
     private ngZone: NgZone, 
-    private geoCoder: MapGeocoder
-  ) { }
+    private geoCoder: MapGeocoder,
+    public platofrm: Platform,
+  ) { 
+    this.platofrm.ready().then(()=>{
+      this.DistanceCalculation();
+    })
+  }
 
   gotoresult2(){
     console.log('hello world')
      //this.router.navigateByUrl('/result2')
     //this.router.navigate(['/result2'])
+  }
+
+  FindingDistance(){
+
+    const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix service
+      const matrixOptions = {
+        origins: ["Delft"], // technician locations
+        destinations: ["Rotterdam"], // customer address
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+      };
+      // Call Distance Matrix service
+      service.getDistanceMatrix(matrixOptions, callback);
+
+      // Callback function used to process Distance Matrix response
+      function callback(response, status) {
+        // if (status !== "OK") {
+        //   alert("Error with distance matrix");
+        //   return;
+        // }
+        console.log(response);        
+      }
+
+    // const service = new google.maps.DistanceMatrixService();
+
+    //   // build request
+    // const origin1 = { lat: 55.93, lng: -3.118 };
+    // const origin2 = "Greenwich, England";
+    // const destinationA = "Stockholm, Sweden";
+    // const destinationB = { lat: 50.087, lng: 14.421 };
+
+    // const request = {
+    //   origins: [origin1, origin2],
+    //   destinations: [destinationA, destinationB],
+    //   travelMode: google.maps.TravelMode.DRIVING,
+    //   unitSystem: google.maps.UnitSystem.METRIC,
+    //   avoidHighways: false,
+    //   avoidTolls: false,
+    // };
+
+    // console.log(service.getDistanceMatrix(request).then((response) => {
+    //   // put response
+    //   (document.getElementById("response") as HTMLDivElement).innerText =
+    //     JSON.stringify(response, null, 2)})
+
+    // )
+
+    //   // put request on page
+    // (document.getElementById("request") as HTMLDivElement).innerText =
+    // JSON.stringify(request, null, 2);
+
+  //     // get distance matrix response
+  //   service.getDistanceMatrix(request).then((response) => {
+  //   // put response
+  //   (document.getElementById("response") as HTMLDivElement).innerText =
+  //     JSON.stringify(response, null, 2);
+  };
+
+  DistanceCalculation(){
+    console.log(
+      ('the distance is')
+    )
+    // var gps1 = new google.maps.LatLng(52.012093,4.360011)
+    // var gps2 = new google.maps.LatLng(52.069721,4.322500)
+    // var distanceinmeter = google.maps.geometry.spherical.computeDistanceBetween(gps1,gps2);
+    // alert(distanceinmeter);
   }
 
   ionViewDidEnter(){
